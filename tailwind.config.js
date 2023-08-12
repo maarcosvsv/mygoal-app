263dd89ab61d8f48ae119cf2ad6efcf6f77074e3
maarcosvsv <maarcosvsv@gmail.com>
2023-08-12 17:52:24 -0300

    The first commit of projects including base and Login page.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.jsx'],
+    theme: {
+        colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'white': '#ffffff',
+                'purple': '#3f3cbb',
+                'midnight': '#121063',
+                'metal': '#565584',
+                'tahiti': '#3ab7bf',
+                'silver': '#ecebff',
+                'bubble-gum': '#ff77e9',
+                'bermuda': '#78dcca',
+                'silver-teal': '#00adb5',
+                'hover-silver-teal': '#00B5BD'
+        },
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/forms')],
+};
+
